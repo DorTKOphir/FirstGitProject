307c073b143809fc0b018c3819cbff130461daab
DorTKOphir <dor.ophir5@gmail.com>
2020-09-16 14:13:55 +0300

    Create Piano.java

---
+++ Piano.java
@@ -0,0 +1,49 @@
+package guitarshop;
+
+public class Piano {
+	private int id;
+	private int octave;
+	private String manufacturer;
+	private int price;
+	private static int count = 1000;
+	
+	public Piano(int octave, String manufacturer, int price)
+	{
+		this.id = count;
+		count++;
+		this.octave = octave;
+		this.manufacturer = manufacturer;
+		this.price = price;
+	}
+	
+	public int getOctave() {
+		return octave;
+	}
+
+	public void setOctave(int octave) {
+		this.octave = octave;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Piano - Id = " + id + ", Octave = " + octave + ", Manufacturer = " + manufacturer + ", Price = " + price;
+	}
+	
+	
+}
